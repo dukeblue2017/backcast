c4f19ed1a7cc15ad92a4da2fbe2da50276404d0a
Hack Reactor Student <student@hackreactor.com>
2017-10-16 17:35:06 -0500

    Dinner Break

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,9 +1,25 @@
 var VideoListView = Backbone.View.extend({
+  el: '.list',
 
+  initialize: function() {
+    this.render()
+  },
 
   render: function() {
+    // this.collection.forEach(this.renderMovie, this);
+    console.log('this in VideoListEntryView: ', this)
+
     this.$el.children().detach();
+
     this.$el.html(this.template());
+
+    this.collection.forEach((video) => {
+      //Make a new VideoListEntryView
+      console.log('this should be a video model: ', video)
+      // new VideoListEntryView(video)
+    })
+
+
     return this;
   },
 
